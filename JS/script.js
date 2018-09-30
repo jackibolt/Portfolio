9ee7c3556614e07ee6cb77a54d53392bf75af10b
Jacki Bolt <bolt.jacqueline@gmail.com>
2018-09-30 00:44:57 -0400

    most of layout

--- JS/script.js
+++ JS/script.js
@@ -4,11 +4,18 @@ console.log('it works');
 const nav = document.querySelector('nav');
 const bigContainer = document.getElementById('big-container');
 
-// Show nav item titles for medium to large screens
+
+if (screen.width < 768) {
+
+}
+
+
+// Show nav item titles for large and xl screens
 nav.addEventListener('mouseover', (e) => {
   if (screen.width > 767) {
 
     if (e.target.textContent === '1') {
+      console.log(e.target);
       e.target.textContent = 'TBD';
       e.target.addEventListener('mouseout', () => {
         e.target.textContent = '1';
@@ -35,46 +42,95 @@ nav.addEventListener('mouseover', (e) => {
   }
 });
 
+
+// HTML content variables for content box
+const aboutHTML = `
+  <div class="container-fluid about">
+    <h1 class='mb-3 mt-4 text-left title'>About Me</h1>
+    <div class="py-4 text-left about-text">
+      <h4 class='mb-5 ml-3 ml-md-4'>I love helping good ideas get off the ground.<img src='IMGS/biplane.svg' alt='biplane' id='biplane' class='text-icon'></h4>
+      <p>Behind every great idea, there's someone dreaming. Whether you're solving problems, telling stories or selling products, you need a web presence that connects your dreams to the world.</p>
+      <p>My goal is build creative user experiences that connect <span class='humans'>humans to humans</span>, and make the world a little <span id='bright'>brighter</span>.</p>
+    </div>
+    <img src='IMGS/light-bulb.svg' alt='light bulb' class='text-icon' id='lightbulb'>
+  </div>
+  `;
+
 const projectsHTML = `
   <div class="container-fluid projects">
-    <h3 class='mb-3 text-left title'>Stuff I Built</h3>
+    <h3 class='mb-3 mt-4 text-left title'>Stuff I Built</h3>
     <div class='col container container-fluid text-center'>
       <div class="row justify-content-center">
         <div class="col-lg m-1 m-md-2 m-lg-3 p-0 a">
           <a href="http://www.jordanbolt.com" target='_blank'><img src="IMGS/jordan_pic.png" alt="jordanbolt.com"></a>
-        </div>
-        <div class="col-lg m-1 m-md-2 m-lg-3 p-0 b">
-          <img src="IMGS/api_directory.png" alt="API Directory">
+          <div class="project-overlay a1">
+            <h5>Cinamatography Portfolio</h5>
+            <p class='desc'>Online portfolio for a sports cinematographer.</p>
+          <p><span class='skill'>HTML</span>   <span class='skill'>CSS</span></p>
         </div>
       </div>
-      <div class="row justify-content-center">
-        <div class="col-lg m-1 m-md-2 m-lg-3 p-0 a">
-          <img src="IMGS/photo_gallery.png" alt="Searchable Photo Gallery">
+      <div class="col-lg m-1 m-md-2 m-lg-3 p-0 b">
+        <img src="IMGS/api_directory.png" alt="API Directory">
+        <div class="project-overlay b1">
+          <h5>Employee Directory</h5>
+          <p class='desc'>Searchable directory of employee information.</p>
+          <p><span class='skill'>Javascript</span>   <span class='skill'>API</span>   <span class='skill'>HTML</span>   <span class='skill'>CSS</span></p>
         </div>
-        <div class="col-lg m-1 m-md-2 m-lg-3 p-0 b">
-          <img src="IMGS/webapp.png" alt="WebApp">
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-lg m-1 m-md-2 m-lg-3 p-0 a">
+        <img src="IMGS/photo_gallery.png" alt="Searchable Photo Gallery">
+        <div class="project-overlay a1">
+          <h5>Photo Gallery</h5>
+          <p class='desc'>Searchable photo gallery.</p>
+          <p><span class='skill'>HTML</span> <span class='skill'>CSS</span> <span class='skill'>Javascript</span> <span class='skill'>JQuery</span></p>
+        </div>
+      </div>
+      <div class="col-lg m-1 m-md-2 m-lg-3 p-0 b">
+        <img src="IMGS/webapp.png" alt="WebApp">
+        <div class="project-overlay b1">
+          <h5>WebApp User Dashboard</h5>
+          <p class='desc'>Dashboard for a generic webapp.</p>
+          <p><span class='skill'>HTML</span> <span class='skill'>CSS</span> <span class='skill'>Javascript</span> <span class='skill'>JQuery</span></p>
         </div>
       </div>
     </div>
   </div>
+</div>
   `;
 
 const contactHTML = `
-  <div class="container-fluid contact">
-    <h3 class='mb-3 text-left title'>Connect with me</h3>
-    <div class="py-4 text-left contact-text">
-      <p>I'm currently looking for opportunities to grow my portfolio and try new things.</p>
+  <div class="col container-fluid contact text-left">
+    <h3 class='mb-3 mt-4 text-left title'>Connect with me</h3>
+    <div class="py-4 ml-4 text-left contact-text">
+      <p>I'm currently looking for opportunities to grow my portfolio <br>and try new things.</p>
       <p class='mb-5'>Want to build something great with me?</p>
-      <a href="mailto:bolt.jacqueline@gmail.com?" target="_top">bolt.jacqueline@gmail.com</a>
     </div>
+    <a href="mailto:bolt.jacqueline@gmail.com?" target="_top" class='text-left'>bolt.jacqueline@gmail.com</a>
   </div>
   `;
 
 
-
+// displays content for each menu section
 nav.addEventListener('click', (e) => {
   console.log(e.target);
-  if (e.target.id === '3' || e.target.textContent === '3' || e.target.textContent === 'Projects') {
+  if (e.target.id === '2' || e.target.textContent === '2' || e.target.textContent === 'About') {
+    bigContainer.innerHTML = aboutHTML;
+
+    //shows light bulb svg
+    const bright = document.querySelector('#bright');
+    const lightbulb = document.querySelector('#lightbulb');
+
+    bright.addEventListener('mouseover', () => {
+      lightbulb.style.display = 'inline';
+      bright.addEventListener ('mouseout', () => {
+        lightbulb.style.display = 'none';
+      });
+    });
+
+  }
+  else if (e.target.id === '3' || e.target.textContent === '3' || e.target.textContent === 'Projects') {
     bigContainer.innerHTML = projectsHTML;
   }
   else if (e.target.id === '4' || e.target.textContent === '4' || e.target.textContent === 'Contact') {
